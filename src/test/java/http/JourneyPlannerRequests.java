ec9d4e90de895455d1afe74b16b8ab4c9f4ba82d
iminder.rehal <iminder.rehal@bottomline.com>
2022-08-20 14:46:05 +0100

    Removing unused code

--- src/test/java/http/JourneyPlannerRequests.java
+++ src/test/java/http/JourneyPlannerRequests.java
@@ -1,53 +1,23 @@
 package http;
 
 import com.github.sitture.env.config.EnvConfig;
+import kong.unirest.HttpResponse;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import kong.unirest.HttpResponse;
-
 public class JourneyPlannerRequests extends Requests {
-
-    private final String ACCEPT = "Accept";
-    private final String CONTENT_TYPE  = "Content-Type";
-    private final String ACCEPT_DEFAULT = "*/*";
-    private final String APPLICATION_JSON = "application/json";
-    private final String APP_KEY = "app_key";
-    private final String APP_KEY_VALUE = EnvConfig.get("APP_KEY");
 
     public JourneyPlannerRequests() {
         super(EnvConfig.get("HOST"));
     }
 
-//    public HttpResponse get(String path) {
-//        HashMap<String, String> headers = new HashMap<>();
-//        headers.put(ACCEPT, "*/*");
-//        return getRequest(path, headers);
-//    }
-
-//    public HttpResponse get(String path, Map<String, String> headers) {
-//        return getRequest(path, headers);
-//    }
-
-    public HttpResponse get(final String path, Map<String, Object> queryParams) {
+    public HttpResponse<?> get(final String path, Map<String, Object> queryParams) {
         HashMap<String, String> headers = new HashMap<>();
-        headers.put(ACCEPT, ACCEPT_DEFAULT);
-        headers.put(CONTENT_TYPE, APPLICATION_JSON);
-        headers.put(APP_KEY, APP_KEY_VALUE);
+        headers.put("Accept", "*/*");
+        headers.put("Content-Type", "application/json");
         headers.put("Cache-Control", "no-cache");
+        queryParams.put("app_key", EnvConfig.get("APP_KEY"));
         return getRequest(path, headers, queryParams);
     }
-
-//    public HttpResponse post(String path, String body) {
-//        HashMap<String, String> headers = new HashMap<>();
-//        headers.put(ACCEPT, ACCEPT_DEFAULT);
-//        headers.put(CONTENT_TYPE, APPLICATION_JSON);
-//        return postRequest(path, headers, body);
-//    }
-//
-//    public HttpResponse post(String path, Map<String, String> headers, String body) {
-//        return postRequest(path, headers, body);
-//    }
-
 }
--- src/test/java/http/Requests.java
+++ src/test/java/http/Requests.java
@@ -1,18 +1,9 @@
 package http;
 
 import com.thoughtworks.gauge.Gauge;
-
-import org.apache.commons.codec.binary.Base64;
+import kong.unirest.*;
 
 import java.util.Map;
-
-import kong.unirest.GetRequest;
-import kong.unirest.HttpRequest;
-import kong.unirest.HttpRequestWithBody;
-import kong.unirest.HttpResponse;
-import kong.unirest.MultipartBody;
-import kong.unirest.Unirest;
-import kong.unirest.UnirestException;
 
 public abstract class Requests {
 
@@ -21,36 +12,14 @@ public abstract class Requests {
         STRING
     }
 
-    private String host;
-
-    public String getHost() {
-        return this.host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
+    private final String host;
     public Requests(String host) {
         this.host = host;
     }
 
-//    public HttpResponse<?> getRequest(String path, Map<String, String> headers) {
-//        return this.getRequest(path, headers, Type.JSON);
-//    }
-
     public HttpResponse<?> getRequest(String path, Map<String, String> headers, Map<String, Object> queryParams) {
         return this.getRequest(path, headers, queryParams, Type.JSON);
     }
-
-//    public HttpResponse<?> getRequestAsString(String path, Map<String, String> headers) {
-//        return this.getRequest(path, headers, Type.STRING);
-//    }
-
-//    public HttpResponse<?> getRequest(String path, Map<String, String> headers, Type type) {
-//        HttpRequest<?> request = Unirest.get(this.host + path).headers(headers);
-//        return this.send(request, type);
-//    }
 
     public HttpResponse<?> getRequest(String path, Map<String, String> headers, Map<String, Object> queryString, Type type) {
         HttpRequest<?> request = Unirest.get(this.host + path).queryString(queryString).headers(headers);
@@ -72,103 +41,5 @@ public abstract class Requests {
         } catch (UnirestException e) {
             throw new RuntimeException(String.format("Error making the request. \n%s", e.getMessage()));
         }
-    }
-
-    private HttpResponse<?> bodyRequest(RequestType requestType, String path, Map<String, String> headers, String body, Type type) {
-        HttpRequest<?> request = ((HttpRequestWithBody)requestType.method(this.host + path).headers(headers)).body(body);
-        return this.send(request, type);
-    }
-
-    private HttpResponse<?> fieldsRequest(RequestType requestType, String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
-        MultipartBody request = ((HttpRequestWithBody)requestType.method(this.host + path).headers(headers)).fields(fields);
-        return this.send(request, type);
-    }
-
-    private HttpResponse<?> optionsRequest(String path, Map<String, String> headers, Type type) {
-        GetRequest request = (GetRequest)Unirest.options(this.host + path).headers(headers);
-        return this.send(request, type);
-    }
-
-    private HttpResponse<?> noBodyRequest(RequestType requestType, String path, Map<String, String> headers, Type type) {
-        HttpRequest<?> request = requestType.method(this.host + path).headers(headers);
-        return this.send(request, type);
-    }
-
-    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, String body, Type type) {
-        return this.bodyRequest(Unirest::delete, path, headers, body, type);
-    }
-
-    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
-        return this.fieldsRequest(Unirest::delete, path, headers, fields, type);
-    }
-
-    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, String body) {
-        return this.deleteRequest(path, headers, body, Type.JSON);
-    }
-
-    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
-        return this.deleteRequest(path, headers, fields, Type.JSON);
-    }
-
-    protected HttpResponse<?> deleteRequest(String path, Map<String, String> headers) {
-        return this.noBodyRequest(Unirest::delete, path, headers, Type.JSON);
-    }
-
-    protected HttpResponse<?> postRequest(String path, Map<String, String> headers) {
-        return this.noBodyRequest(Unirest::post, path, headers, Type.JSON);
-    }
-
-    protected HttpResponse<?> postRequest(String path, Map<String, String> headers, Type type) {
-        return this.noBodyRequest(Unirest::post, path, headers, type);
-    }
-
-    public HttpResponse<?> postRequest(String path, Map<String, String> headers, String body, Type type) {
-        return this.bodyRequest(Unirest::post, path, headers, body, type);
-    }
-
-    public HttpResponse<?> postRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
-        return this.fieldsRequest(Unirest::post, path, headers, fields, type);
-    }
-
-    public HttpResponse<?> postRequest(String path, Map<String, String> headers, String body) {
-        return this.postRequest(path, headers, body, Type.JSON);
-    }
-
-    public HttpResponse<?> postRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
-        return this.postRequest(path, headers, fields, Type.JSON);
-    }
-
-    public HttpResponse<?> putRequest(String path, Map<String, String> headers) {
-        return this.noBodyRequest(Unirest::put, path, headers, Type.JSON);
-    }
-
-    public HttpResponse<?> putRequest(String path, Map<String, String> headers, String body, Type type) {
-        return this.bodyRequest(Unirest::put, path, headers, body, type);
-    }
-
-    public HttpResponse<?> putRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
-        return this.fieldsRequest(Unirest::put, path, headers, fields, type);
-    }
-
-    public HttpResponse<?> putRequest(String path, Map<String, String> headers, String body) {
-        return this.putRequest(path, headers, body, Type.JSON);
-    }
-
-    public HttpResponse<?> putRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
-        return this.putRequest(path, headers, fields, Type.JSON);
-    }
-
-    protected HttpResponse<?> optionsRequest(String path, Map<String, String> headers) {
-        return this.optionsRequest(path, headers, Type.JSON);
-    }
-
-    public String getBasicAuthFromCreds(String auth) {
-        byte[] encodedBytes = Base64.encodeBase64(auth.getBytes());
-        String var10000 = new String(encodedBytes);
-        return "Basic " + var10000;
-    }
-
-    private interface RequestType {
-        HttpRequestWithBody method(String var1);
     }
 }
